5bc99cec2fa3147fc34c4f9cf41a9f0533011d15
Hansiqii <1148008999@qq.com>
2025-10-24 22:15:34 +0800

    删掉main中不需要的注释

--- main.py
+++ main.py
@@ -22,22 +22,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model.to(device)
 
-# 试图多卡结果不好
-# if torch.cuda.device_count() > 1:
-#    print(f"Using {torch.cuda.device_count()} GPUs! (DataParallel)")
-#    model = torch.nn.DataParallel(model)
-
-# optimizer = torch.optim.LBFGS(model.parameters(), lr=1, max_iter=20, max_eval=None, tolerance_grad=1e-07, tolerance_change=1e-09, history_size=10, line_search_fn= "strong_wolfe")
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-# optimizer = torch.optim.Adam([
-#     {'params': [model.delta_y_1, model.delta_x_2, model.delta_z_1, model.delta_z_2, model.delta_x_3, model.delta_y_3,
-#                 model.screen_y_1, model.screen_x_2, model.screen_z_1, model.screen_z_2, model.screen_x_3, model.screen_y_3],
-#      'lr': 5e-4},
-#     {'params': [param for name, param in model.named_parameters() if name not in [
-#         'delta_y_1', 'delta_x_2', 'delta_z_1', 'delta_z_2', 'delta_x_3', 'delta_y_3',
-#         'screen_y_1', 'screen_x_2', 'screen_z_1', 'screen_z_2', 'screen_x_3', 'screen_y_3']],
-#      'lr': 1e-4}
-# ])
+
 torch.save(
     {
         "model.state_dict": model.state_dict(),
